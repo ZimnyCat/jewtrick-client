668d567c4dcd4a1597942f1d7be82ff3fd038e08
ZimnyCat <58877213+ZimnyCat@users.noreply.github.com>
2020-08-30 22:06:58 +0500

    add lost connection bypass

--- jew_trick_client.py
+++ jew_trick_client.py
@@ -1,12 +1,15 @@
 from time import sleep
+from mcstatus import MinecraftServer
 import requests
 
 requests_counter = 0
+tbtt = MinecraftServer.lookup("2b2t.org")
+
 print("─╔╦═╦╦═╦╗╔══╦═╦══╦═╦╦╗")
 print("─║║╦╣║║║║╚╗╔╣╬╠║║╣╔╣╔╝")
 print("╔╣║╩╣║║║║─║║║╗╬║║╣╚╣╚╗")
 print("╚═╩═╩═╩═╝─╚╝╚╩╩══╩═╩╩╝")
-print("jew trick (client) v1.2")
+print("jew trick (client) v1.3-beta")
 print("https://jewtrick.ml")
 print("")
 motd = requests.get("https://jewtrick.ml/server/motdv11.html")
@@ -14,12 +17,16 @@ if motd.status_code == 200:
     print(motd.text)
 print("")
 while True:
+    request_delay = 1
     requests_counter = requests_counter + 1
     try:
         jew_int = int(requests.get("https://jewtrick.ml/server/jewtrickstatus.html").text)
         if jew_int == 2:
+            request_delay = 2
             jew_online = requests.get("https://jewtrick.ml/server/jew_online.html").text
+            online = tbtt.status().players.online
             print("JEW TRICK TIME!!! Онлайн 2b2t:", jew_online, "(" + str(requests_counter) + ")")
+            print("Запрос на 2b2t отправлен. Онлайн:", online)
         elif jew_int == 1:
             print("jew trick сейчас невозможен (" + str(requests_counter) + ")")
         elif jew_int == 0:
@@ -29,4 +36,4 @@ while True:
             print("Сервер jew trick получает онлайн 2b2t (" + str(requests_counter) + ")")
         else:
             print("Сервер jew trick недоступен (" + str(requests_counter) + ")")
-    sleep(1)
+    sleep(request_delay)
